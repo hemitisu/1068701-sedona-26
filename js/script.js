8757d79be27f3425b0efdda8a5e006af6b2d82e6
hemitisu <54809895+hemitisu@users.noreply.github.com>
2019-10-18 18:17:00 +0700

    Добавил покачивание формы

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 var viewButton=document.querySelector('.form-view-button');
 var modal=document.querySelector('.hostel-search-form');
+var checkIn = modal.querySelector("[name=check-in]");
+var checkOut = modal.querySelector("[name=check-out]");
+var X=0;
 
 
 viewButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
+     evt.preventDefault();
+    if (X>0){
+      modal.classList.add("modal-hide");
+      X=0;
+    }
+    else{
+      modal.classList.remove("modal-error");
     if(modal.classList.contains("modal-hide")){
     modal.classList.remove("modal-hide");
     modal.classList.add("modal-show");
@@ -12,5 +21,17 @@ viewButton.addEventListener("click", function (evt) {
     modal.classList.remove("modal-show");
     modal.classList.add("modal-hide");
     }
+    }
   });
 
+modal.addEventListener("submit", function (evt) {
+    if (!checkIn.value || !checkOut.value) {
+      evt.preventDefault();
+      modal.classList.remove("modal-hide");
+      modal.classList.remove("modal-show");
+      modal.classList.remove("modal-error");
+      modal.offsetWidth = modal.offsetWidth;
+      modal.classList.add("modal-error");
+      X=X+1;
+  }
+});
